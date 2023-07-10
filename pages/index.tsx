a106851aceddfa1f59a4dcdfb81a3011ed663569
DESKTOP-39CBMEP\User <nara20210617@gmail.com>
2023-07-10 15:43:55 +0900

    feat: 상세페이지 제작중

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,48 +15,16 @@ import { marked } from "marked";
 
 export default function Page({ title, content }) {
   console.log(title, content);
-  // const mdText = `
-  // # Our Project
 
-  // Hello, **Markdown!**.
-  // `;
-
-  // const mdAst = unified().use(markdown).parse(mdText);
-  // const html_text = unified()
-  //   .use(markdown)
-  //   .use(remark2rehype)
-  //   .use(html)
-  //   .processSync(mdText);
-
-  // console.log(mdAst);
-  // console.log(html_text.toString());
-
-  // const result = unified()
-  //   .use(remarkParse)
-  //   .use(remarkRehype)
-  //   .use(rehypeStringify)
-  //   .processSync("## Hello, *World*!");
-
-  // console.log(result.value);
-  // const [test, setTest] = useState(result.value);
-
-  // function createMarkup() {
-  //   return { __html: result.value };
-  // }
   return (
     <div>
       <h1>홈</h1>
       <div dangerouslySetInnerHTML={{ __html: title }} />
-
-      {/* <div dangerouslySetInnerHTML={{ __html: test }} />
-      <div dangerouslySetInnerHTML={createMarkup()} />
-      <div dangerouslySetInnerHTML={{ __html: html_text.toString() }} /> */}
     </div>
   );
 }
 
 export async function getStaticProps() {
-  //const list = ["__posts/text.md", "__posts/second.md"];
   const filePath = path.join(process.cwd(), "__posts/text.md");
   const fileContents = fs.readFileSync(filePath, "utf8");
   const { content, data } = matter(fileContents);
